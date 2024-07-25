3b449caae31eaee2d50d2ea308ba0b71573b18c8
WINNIE.X <winnie.xiao@sjfood.com>
2024-07-25 17:05:36 +0800

    Init skill managment ui
    Init skill managment ui

---
+++ web/src/pages/skill/hook.ts
@@ -0,0 +1,115 @@
+export const useAction = () => {
+  const skillCard = [
+    {
+      name: "王嘉爾",
+      type: "問答類",
+      skillID: "232323",
+      createTime: "2099-9-9",
+      state: "訓練中",
+    },
+    {
+      name: "aaaa",
+      type: "問答類",
+      skillID: "232323",
+      createTime: "2099-9-9",
+      state: "訓練中",
+    },
+    {
+      name: "王嘉爾",
+      type: "表格類",
+      skillID: "232323",
+      createTime: "2099-9-9",
+      state: "已完成",
+    },
+    {
+      name: "王嘉爾",
+      type: "問答類",
+      skillID: "232323",
+      createTime: "2099-9-9",
+      state: "訓練中",
+    },
+    {
+      name: "鹿晗",
+      type: "知識類",
+      skillID: "232323",
+      createTime: "2099-9-9",
+      state: "訓練中",
+    },
+    {
+      name: "王嘉爾",
+      type: "表格類",
+      skillID: "232323",
+      createTime: "2099-9-9",
+      state: "訓練中",
+    },
+    {
+      name: "lololo",
+      type: "問答類",
+      skillID: "232323",
+      createTime: "2099-9-9",
+      state: "訓練中",
+    },
+    {
+      name: "鹿晗",
+      type: "知識類",
+      skillID: "232323",
+      createTime: "2099-9-9",
+      state: "訓練中",
+    },
+    {
+      name: "周杰倫",
+      type: "表格類",
+      skillID: "232323",
+      createTime: "2099-9-9",
+      state: "訓練中",
+    },
+    {
+      name: "王嘉爾",
+      type: "問答類",
+      skillID: "232323",
+      createTime: "2099-9-9",
+      state: "訓練中",
+    },
+    {
+      name: "hao",
+      type: "問答類",
+      skillID: "232323",
+      createTime: "2099-9-9",
+      state: "訓練中",
+    },
+    {
+      name: "王嘉爾",
+      type: "表格類",
+      skillID: "232323",
+      createTime: "2099-9-9",
+      state: "訓練中",
+    },
+    {
+      name: "王嘉爾",
+      type: "問答類",
+      skillID: "232323",
+      createTime: "2099-9-9",
+      state: "訓練中",
+    },
+    {
+      name: "ddddd",
+      type: "問答類",
+      skillID: "232323",
+      createTime: "2099-9-9",
+      state: "訓練中",
+    },
+    {
+      name: "王嘉爾",
+      type: "表格類",
+      skillID: "232323",
+      createTime: "2099-9-9",
+      state: "訓練中",
+    },
+  ];
+
+  const onChange = (pageNumber) => {
+    console.log("Page: ", pageNumber);
+  };
+
+  return { skillCard, onChange };
+};
